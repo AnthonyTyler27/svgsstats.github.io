90b969faee107409c6003648488354627483eb5b
atyler <tylersvgs@gmail.com>
2018-08-11 10:09:24 -0400

    load new data
    hope this works

--- class.R
+++ class.R
@@ -1,3 +1,5 @@
 installPackages <- function(packs) {
   if (!packs %in% installed.packages()) install.packages(packs)
 }
+
+source("https://svgsstats.github.io/Presentations/ggplot2_other_graphs/load.R")
